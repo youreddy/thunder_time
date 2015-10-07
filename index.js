46ec44346196adeddb6d14c5179dd0c40f501ac5
kimeberz <kimeberz@gmail.com>
2015-10-07 11:31:32 -0700

    more stuff for new file paths

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').Server(app);
 // express has defined the method get
 var io = require('socket.io')(http);
+var path = require('path');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
