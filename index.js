8b724019991616f9d7dc9065ecf29fa3cf7b58b9
kimeberz <kimeberz@gmail.com>
2015-10-08 17:42:09 -0700

    urv and i deployed

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+app.set('port', process.env.PORT || 3000);
+
+http.listen(app.get('port'), function(){
   console.log('listening on *:3000');
 });
